f7f8d3fd277d4cc3d4ed4a2e80adcdfa7fe653c3
r-aoki <ryota.aoki@it-forest.com>
2013-11-27 22:44:42 +0900

    eqlipseで作業出来ているのかテストのための変更

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -7,13 +7,11 @@ import java.util.regex.Pattern;
 /**
  * 適当に何か貼り付けて実行するクラス
  * 内容はその時々で変わる
+ * 現状:eqlipseとの連携を確認するために修正
  * @author r-aoki
  */
 public class Hello {
   public static void main(String[] args) throws ParseException {
-	Pattern pattern = Pattern.compile("[\\-\\-s]");
-	Matcher matcher = pattern.matcher("\n");
-	
-	System.out.println(matcher.matches());
+      System.out.println("Hello, Git.");
   }
 }
